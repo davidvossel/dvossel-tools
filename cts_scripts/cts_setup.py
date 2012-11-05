160696c8edc777fd780a37000f797d034c78b599
David Vossel <dvossel@redhat.com>
2012-11-05 17:54:57 -0600

    move launch scripts

--- cts_scripts/cts_setup.py
+++ cts_scripts/cts_setup.py
@@ -12,6 +12,8 @@ class TestOptions:
 		self.options['build'] = "true"
 		self.options['nodes'] = ""
 		self.options['stack'] = "coro"
+		self.options['launch_script'] = ""
+		self.options['launch_parameters'] = ""
 
 	def build_options(self, argv):
 		args = argv[1:]
@@ -26,7 +28,8 @@ class TestOptions:
 				self.options['build'] = args[i+1]
 				skip = 1
 			elif args[i] == "-s":
-				self.options['src-dir'] = 1
+				self.options['src-dir'] = args[i+1]
+				skip = 1
 			elif args[i] == "-n":
 				self.options['nodes'] = args[i+1]
 				print "installing on nodes %s" % self.options['nodes']
@@ -34,13 +37,22 @@ class TestOptions:
 			elif args[i] == "-c":
 				self.options['stack'] = args[i+1]
 				skip = 1
+			elif args[i] == "-l":
+				self.options['launch_script'] = args[i+1]
+				skip = 1
+			elif args[i] == "-p":
+				self.options['launch_parameters'] = args[i+1]
+				skip = 1
+
 	def show_usage(self):
 		print "usage: " + sys.argv[0] + " [options]"
 		print "Options:"
 		print "\t [-n]                       nodes"
-		print "\t [-s]                     src dir"
+		print "\t [-s]                       src dir"
 		print "\t [-b]                       build"
 		print "\t [-c]                       stack"
+		print "\t [-p]                       launch parameters"
+		print "\t [-l]                       launch script" ## finish this
 
 def main(argv):
 	o = TestOptions()
@@ -48,7 +60,7 @@ def main(argv):
 
 	#build rpms
 	if o.options['build'] == "true":
-		builder = subprocess.Popen(shlex.split("./make_rpms %s" % o.options['src-dir']))
+		builder = subprocess.Popen(shlex.split("helper_scripts/make_rpms %s" % o.options['src-dir']))
 		builder.wait()
 
 	#clean nodes and send rpms out
@@ -57,15 +69,25 @@ def main(argv):
 		for node in nodes:
 			run_clean(node, o)
 
+	# launch cts if we have a script and parameters
+	if o.options['launch_script'] and o.options['launch_parameters']:
+		cmd = ("launch_scripts/%s %s" % (o.options['launch_script'], o.options['launch_parameters']))
+		cts = subprocess.Popen(shlex.split(cmd))
+		cts.wait()
+
 def run_clean(host, o):
 
-	prefix = os.getcwd()
+	curdir = os.getcwd();
+	prefix = "%s/helper_scripts" % curdir
+
 	os.system("python %s/kill_cluster.py %s" % (prefix, host))
 	os.system("python %s/clear_cluster_cache.py %s" % (prefix, host))
-	os.system("python %s/install_cluster_rpms.py pacemaker_rpms %s" % (prefix, host))
+	os.system("python %s/install_cluster_rpms.py %s/pacemaker_rpms %s" % (prefix, curdir, host))
+
 	if o.options['stack'] == "coro":
 		os.system("python %s/corosync2_cfg_gen.py %s" % (prefix, o.options['nodes']))
 		os.system("scp corosync.conf %s:/etc/corosync/corosync.conf" % (host))
+
 	os.system("python %s/mis_setup.py %s" % (prefix, host))
 
 if __name__=="__main__":
